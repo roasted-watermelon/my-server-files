956a3010b5fa7fc39eaba33b457e8fc0ef6f036a
roasted-watermelon <156080803+roasted-watermelon@users.noreply.github.com>
2024-06-28 12:12:37 +0530

    Add network_finder.sh
    This finds veth network and docker network id sometimes visible on grafana

---
+++ network_finder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for container in $(docker ps -q); do
+    iflink=`docker exec -it $container sh -c 'cat /sys/class/net/eth0/iflink' 2>/dev/null`
+    iflink="${iflink//[[:space:]]/}"
+    container_name=`docker ps -a | grep $container | awk '{print $NF}'`
+    veth="-----------"
+    if [[ $iflink =~ ^[0-9]+$ ]]; then
+      iflink=`echo $iflink|tr -d '\r'`
+      veth=`grep -l $iflink /sys/class/net/veth*/ifindex`
+      veth=`echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;'`
+    fi
+    network_name=`docker inspect $container --format='{{json .NetworkSettings.Networks }}' | jq -r 'keys[]'`
+    network_id=`docker network ls -f "name=$network_name" | tail -1 | awk '{print $1}'`
+    if [[ $network_id == "NETWORK" ]]; then
+      network_id="------------"
+    fi
+    echo $container:$veth:$network_id:$container_name
+done
