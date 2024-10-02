d36414058f2fd9ba8ae7917c2cc08d97a7d6cd06
roasted-watermelon <156080803+roasted-watermelon@users.noreply.github.com>
2024-10-02 21:32:04 +0530

    network_unblock.sh support for domains

--- network_unblock.sh
+++ network_unblock.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME="$1"
+DOMAIN="$2"
 
 container_id="$(docker ps -aq -f name=$CONTAINER_NAME | head -1)"
 echo "Container ID for $CONTAINER_NAME - $container_id"
 
 container_ip="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)"
+
+if [[ -n "$DOMAIN" ]]; then
+  echo "Adding exception for IP - $container_ip - $DOMAIN"
+  sudo iptables -I DOCKER-USER -s "$container_ip" -d "$DOMAIN" -j ACCEPT
+  sudo iptables -I DOCKER-USER -d "$container_ip" -s "$DOMAIN" -j ACCEPT
+  sudo iptables -L DOCKER-USER -v -n --line-numbers | grep $container_ip 
+  exit
+fi
 
 echo "Deleting rules for IP - $container_ip"
 
@@ -18,4 +27,3 @@ until false; do
   rule_number="$(echo $rule_line | awk '{print $1}')"
   sudo iptables -D DOCKER-USER $rule_number
 done
-
